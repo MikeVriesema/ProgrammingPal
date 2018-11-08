71302d7e17d9cf13e415084e56a2bcdaf0505fbc
LukeOSG <lukegriffin07@gmail.com>
2018-11-08 18:06:25 +0000

    Website v1.0
    -index.php created 
    -indexStyle.css created
    -form.php created 
    -formStyle.css created

---
+++ form.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Programming Pal</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="formStyle.css">
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+		<link rel="icon" href="favicon.ico">
+		
+	</head>
+
+	<div id ="container">
+		<body>
+			
+		<div id = "header">
+			<h1>Programming Pal
+			<sub>(Destroying our souls so you don't have to)</sub></h1>	
+		</div>
+		
+		<nav>
+			<a href = "index.php">Home</a>
+			<a href = "form.php">Register</a>
+			<a href = "#">Example 3</a>
+			<a href = "#">Example 4</a>
+			<a href = "#">Example 5</a>
+		</nav>	
+	
+		<div id = "content">
+		
+		<h1>Some Stuff</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a pulvinar lectus. Vestibulum pellentesque, ligula in laoreet iaculis, purus nibh laoreet enim, in tempor felis risus ut ex. Vestibulum id venenatis ligula. Integer pellentesque mauris sit amet neque imperdiet tempor. Vivamus interdum semper sapien, et vestibulum sem finibus nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras sapien nibh, laoreet mollis augue vitae, tempus sagittis risus. Etiam in nisi rhoncus, dapibus dolor sed, accumsan elit. Nam tortor nibh, tincidunt a aliquam non, vestibulum eu arcu. Phasellus orci turpis, vestibulum consectetur vestibulum eget, elementum maximus justo. Phasellus tempus ligula nec lorem laoreet, eget sollicitudin orci interdum. Fusce nec viverra dui. Sed congue nunc a consectetur auctor. Donec tempus rhoncus eros. Duis eu rutrum dui. Suspendisse justo dolor, suscipit eu vulputate ac, elementum ac massa.</p>
+
+		<ul>
+			<li>Java</li>
+			<li>C</li>
+			<li>Php</li>
+			<li>HTML</li>
+		</ul>
+
+		<p>Nunc ipsum arcu, mollis sit amet nulla in, interdum fringilla lectus. Curabitur laoreet, dui sed dapibus egestas, mauris urna suscipit sem, quis ullamcorper nisi lorem in nisi. Mauris vel lorem luctus, tempus massa eu, vestibulum ipsum. Sed auctor tincidunt libero a consequat. Quisque elementum nec nunc vel malesuada. Nullam tellus lacus, tincidunt sed pulvinar at, pellentesque et sapien. Duis quis urna eget sem congue sodales. Ut suscipit ante vel nulla efficitur, ut viverra erat aliquam. Sed sagittis ultricies dictum. Aliquam nec pretium est, vel interdum libero. Mauris luctus enim et condimentum scelerisque. Nulla fringilla arcu sit amet sem vulputate aliquam.</p>
+		
+		</div>
+		
+		
+		
+		<div class = "clear"></div>
+		<div id = "footer">
+			<p>Programming Pal,</p>
+			<p>123 UL Street,</p>
+			<p>Co. Limerick</p>
+			<a href="mailto:programming4life@programmingpal.com" target="_top">Send us an email!</a>
+		</div>
+
+			
+		</body>	
+	</div>
+</html>
+
