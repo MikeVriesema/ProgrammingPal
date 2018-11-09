b35e2ae6c5188ffc75b23b023dabe1ba32b10a15
LukeOSG <lukegriffin07@gmail.com>
2018-11-09 22:46:16 +0000

    Version 1.01
    -Updated CSS
    -Edited footer
    -Put in placeholder form for registering users (no php yet)

--- form.php
+++ form.php
@@ -21,24 +21,81 @@
 		<nav>
 			<a href = "index.php">Home</a>
 			<a href = "form.php">Register</a>
-			<a href = "#">Example 3</a>
-			<a href = "#">Example 4</a>
-			<a href = "#">Example 5</a>
+			<a href = "#">Lessons</a>
+			<a href = "#">Testimonials</a>
+			<a href = "#">Contact Us</a>
 		</nav>	
 	
 		<div id = "content">
 		
-		<h1>Some Stuff</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a pulvinar lectus. Vestibulum pellentesque, ligula in laoreet iaculis, purus nibh laoreet enim, in tempor felis risus ut ex. Vestibulum id venenatis ligula. Integer pellentesque mauris sit amet neque imperdiet tempor. Vivamus interdum semper sapien, et vestibulum sem finibus nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras sapien nibh, laoreet mollis augue vitae, tempus sagittis risus. Etiam in nisi rhoncus, dapibus dolor sed, accumsan elit. Nam tortor nibh, tincidunt a aliquam non, vestibulum eu arcu. Phasellus orci turpis, vestibulum consectetur vestibulum eget, elementum maximus justo. Phasellus tempus ligula nec lorem laoreet, eget sollicitudin orci interdum. Fusce nec viverra dui. Sed congue nunc a consectetur auctor. Donec tempus rhoncus eros. Duis eu rutrum dui. Suspendisse justo dolor, suscipit eu vulputate ac, elementum ac massa.</p>
+		<h2>Register Today!</h2>
+		<form method="POST" action ="mailto:17184614@studentmail.ul.ie" id="mainForm">
+		<table id ='formTable'>
+			<tr>
+				<td>		
+					<b><label for="name">Name:</label></b>
+				</td>
+				<td>
+					<input type="text" name="name" id="" tabindex="1" />
+				</td>
+			</tr>
+			
+			<tr>
+				<td>		
+					<b><label for="year">Year:</label></b>
+				</td>	
+				
+				<td>
+					
+					<!-- CODE I FOUND THAT MIGHT BE USEFUL 
+					<select name="years">
 
-		<ul>
-			<li>Java</li>
-			<li>C</li>
-			<li>Php</li>
-			<li>HTML</li>
-		</ul>
+					<?php 
 
-		<p>Nunc ipsum arcu, mollis sit amet nulla in, interdum fringilla lectus. Curabitur laoreet, dui sed dapibus egestas, mauris urna suscipit sem, quis ullamcorper nisi lorem in nisi. Mauris vel lorem luctus, tempus massa eu, vestibulum ipsum. Sed auctor tincidunt libero a consequat. Quisque elementum nec nunc vel malesuada. Nullam tellus lacus, tincidunt sed pulvinar at, pellentesque et sapien. Duis quis urna eget sem congue sodales. Ut suscipit ante vel nulla efficitur, ut viverra erat aliquam. Sed sagittis ultricies dictum. Aliquam nec pretium est, vel interdum libero. Mauris luctus enim et condimentum scelerisque. Nulla fringilla arcu sit amet sem vulputate aliquam.</p>
+					for($i=1; $i<=4; $i++)
+					{
+
+						echo "<option value=".$i.">".$i."</option>";
+					}
+					?> 
+						 <option name="years"> </option>   
+					</select> 
+					
+					
+					<input type="submit" name="submitYears" value="Year" />
+					-->
+					
+					<select name="language" id="language" tabindex="3" multiple="multiple">
+							<option value="java">Java</option>
+							<option value="c">C</option>
+							<option value="bash">Bash</option>
+							<option value="php">Php</option>
+					</select>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<b><label for="userType">UserType:</label></b>
+
+				</td>
+				
+				<td>
+					Student<input type ="radio" name ="userType" id = "userType" tabindex = "4" value = "0"/>
+					<br/>
+					Programmer<input type ="radio" name ="userType" id = "userType" tabindex = "5" value = "1"/>
+				</td>
+			</tr>
+			
+			<td>
+			<br/>
+				<input type ="reset" id ="reset" tabindex ="10" />
+				<input type ="submit" colspan ="2" id ="submit" tabindex ="11"/>
+				
+			</td>
+		</table>
+	</form>
+
 		
 		</div>
 			
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<head>
 		<title>Programming Pal</title>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" type="text/css" href="indexstyle.css">
+		<link rel="stylesheet" type="text/css" href="indexStyle.css">
 		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 		<link rel="icon" href="favicon.ico">
 		
@@ -21,14 +21,14 @@
 		<nav>
 			<a href = "index.php">Home</a>
 			<a href = "form.php">Register</a>
-			<a href = "#">Example 3</a>
-			<a href = "#">Example 4</a>
-			<a href = "#">Example 5</a>
+			<a href = "#">Lessons</a>
+			<a href = "#">Testimonials</a>
+			<a href = "#">Contact Us</a>
 		</nav>	
 	
 		<div id = "content">
 		
-		<h1>Some Stuff</h1>
+		<h2>Why Choose Us?</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a pulvinar lectus. Vestibulum pellentesque, ligula in laoreet iaculis, purus nibh laoreet enim, in tempor felis risus ut ex. Vestibulum id venenatis ligula. Integer pellentesque mauris sit amet neque imperdiet tempor. Vivamus interdum semper sapien, et vestibulum sem finibus nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras sapien nibh, laoreet mollis augue vitae, tempus sagittis risus. Etiam in nisi rhoncus, dapibus dolor sed, accumsan elit. Nam tortor nibh, tincidunt a aliquam non, vestibulum eu arcu. Phasellus orci turpis, vestibulum consectetur vestibulum eget, elementum maximus justo. Phasellus tempus ligula nec lorem laoreet, eget sollicitudin orci interdum. Fusce nec viverra dui. Sed congue nunc a consectetur auctor. Donec tempus rhoncus eros. Duis eu rutrum dui. Suspendisse justo dolor, suscipit eu vulputate ac, elementum ac massa.</p>
 
 		<ul>
@@ -40,17 +40,20 @@
 
 		<p>Nunc ipsum arcu, mollis sit amet nulla in, interdum fringilla lectus. Curabitur laoreet, dui sed dapibus egestas, mauris urna suscipit sem, quis ullamcorper nisi lorem in nisi. Mauris vel lorem luctus, tempus massa eu, vestibulum ipsum. Sed auctor tincidunt libero a consequat. Quisque elementum nec nunc vel malesuada. Nullam tellus lacus, tincidunt sed pulvinar at, pellentesque et sapien. Duis quis urna eget sem congue sodales. Ut suscipit ante vel nulla efficitur, ut viverra erat aliquam. Sed sagittis ultricies dictum. Aliquam nec pretium est, vel interdum libero. Mauris luctus enim et condimentum scelerisque. Nulla fringilla arcu sit amet sem vulputate aliquam.</p>
 		
+		<p>unc ipsum arcu, mollis sit amet nulla in, interdum fringilla lectus. Curabitur laoreet, Nullam tellus lacus, tincidunt sed pulvinar at, pellentesque et sapien. Duis quis urna eget sem congue sodales. Ut suscipit ante vel nulla efficitur, ut viverra erat aliquam. Sed sagittis ultricies dictum. Aliquam nec pretium est, vel interdum libero. Mauris luctus enim et condimentum scelerisque. Nulla fringilla arcu sit amet sem vulputate aliquam. Duis quis urna eget sem congue sodales.</p>
+		
 		</div>
 		
 		<div id = "content2">
 		
-		<h1>Picture of things</h1>
+		<h2>Picture of things</h2>
 		<img src="images/code.jpg" title="Code" width="780" height="400"  alt="Code"></img>
 
 		
 		<p>Sed eget aliquet eros. Morbi condimentum scelerisque pharetra. Mauris id lectus a odio efficitur malesuada. Integer est ex, ultricies sit amet suscipit eget, pulvinar ut leo. Aliquam erat volutpat. Nullam pellentesque fringilla pellentesque. Etiam iaculis felis quis efficitur ultricies. Nunc ac tellus tellus. Cras ultricies nibh elit, convallis euismod turpis pellentesque ac. Donec non nunc dui. Aliquam erat volutpat. Morbi luctus tristique magna, quis bibendum eros ornare sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In id rutrum tortor, vestibulum placerat lectus.</p>
 
 		<p>Nunc ipsum arcu, mollis sit amet nulla in, interdum fringilla lectus. Curabitur laoreet, dui sed dapibus egestas, mauris urna suscipit sem, quis ullamcorper nisi lorem in nisi. Mauris vel lorem luctus, tempus massa eu, vestibulum ipsum. Sed auctor tincidunt libero a consequat. Quisque elementum nec nunc vel malesuada. Nullam tellus lacus, tincidunt sed pulvinar at, pellentesque et sapien. Duis quis urna eget sem congue sodales. Ut suscipit ante vel nulla efficitur, ut viverra erat aliquam. Sed sagittis ultricies dictum. Aliquam nec pretium est, vel interdum libero. Mauris luctus enim et condimentum scelerisque. Nulla fringilla arcu sit amet sem vulputate aliquam.</p>
+	
 		
 		</div>
 		
@@ -60,6 +63,17 @@
 			<p>123 UL Street,</p>
 			<p>Co. Limerick</p>
 			<a href="mailto:programming4life@programmingpal.com" target="_top">Send us an email!</a>
+			
+			<br/>
+			<br/>
+			<a href = "index.php">| Home |</a>
+			
+			<a href = "form.php">| Register |</a>
+			
+			<a href = "#">| Lessons |</a>
+		
+			<a href = "#">| Testimonials |</a>
+			<a href = "#">| Contact Us |</a>
 		</div>
 
 			
