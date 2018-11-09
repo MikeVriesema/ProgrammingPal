0e39a2375d5ab2b5b4a5706d945db9227100487d
NameJeff <37027239+CommanderDolphin@users.noreply.github.com>
2018-11-09 00:23:42 +0000

    ProgrammingPal SQL File
    Updated DB with 2 new tables and inputted data

--- ProgrammingPal.sql
+++ ProgrammingPal.sql
@@ -56,8 +56,10 @@ CREATE TABLE `lessons` (
   PRIMARY KEY (`lessonID`),
   KEY `programmerID` (`programmerID`),
   KEY `languageID` (`languageID`),
+  KEY `userID` (`userID`),
   CONSTRAINT `lessons_ibfk_1` FOREIGN KEY (`programmerID`) REFERENCES `programmers` (`programmerID`),
-  CONSTRAINT `lessons_ibfk_2` FOREIGN KEY (`languageID`) REFERENCES `languages` (`languageID`)
+  CONSTRAINT `lessons_ibfk_2` FOREIGN KEY (`languageID`) REFERENCES `languages` (`languageID`),
+  CONSTRAINT `lessons_ibfk_3` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -71,6 +73,33 @@ LOCK TABLES `lessons` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `prog_langs`
+--
+
+DROP TABLE IF EXISTS `prog_langs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `prog_langs` (
+  `programmerID` int(11) NOT NULL,
+  `languageID` int(11) NOT NULL,
+  KEY `programmerID` (`programmerID`),
+  KEY `languageID` (`languageID`),
+  CONSTRAINT `prog_langs_ibfk_1` FOREIGN KEY (`programmerID`) REFERENCES `programmers` (`programmerID`),
+  CONSTRAINT `prog_langs_ibfk_2` FOREIGN KEY (`languageID`) REFERENCES `languages` (`languageID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prog_langs`
+--
+
+LOCK TABLES `prog_langs` WRITE;
+/*!40000 ALTER TABLE `prog_langs` DISABLE KEYS */;
+INSERT INTO `prog_langs` VALUES (1,1),(1,2),(1,3),(2,1),(2,4),(2,5),(3,2),(3,3),(4,5);
+/*!40000 ALTER TABLE `prog_langs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `programmers`
 --
 
@@ -80,11 +109,8 @@ DROP TABLE IF EXISTS `programmers`;
 CREATE TABLE `programmers` (
   `programmerID` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
-  `languageID` int(11) NOT NULL,
-  PRIMARY KEY (`programmerID`),
-  KEY `languageID` (`languageID`),
-  CONSTRAINT `programmers_ibfk_1` FOREIGN KEY (`languageID`) REFERENCES `languages` (`languageID`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`programmerID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,8 +119,32 @@ CREATE TABLE `programmers` (
 
 LOCK TABLES `programmers` WRITE;
 /*!40000 ALTER TABLE `programmers` DISABLE KEYS */;
-INSERT INTO `programmers` VALUES (1,'Luke',1),(2,'Ryan',5),(3,'Mitch',1),(4,'Gearoid',1);
+INSERT INTO `programmers` VALUES (1,'Luke'),(2,'Mitch'),(3,'Ryan'),(4,'Gearoid');
 /*!40000 ALTER TABLE `programmers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Jeff12'),(2,'DurujiH'),(3,'Alice456');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -106,4 +156,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-08 18:06:54
+-- Dump completed on 2018-11-09  0:20:49
