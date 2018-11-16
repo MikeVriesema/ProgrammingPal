6fbdad5f979ec6cd85154f4eee0acbdd143e5561
NameJeff <37027239+CommanderDolphin@users.noreply.github.com>
2018-11-16 01:39:13 +0000

    Big PHP Bois

--- lessons.php
+++ lessons.php
@@ -54,6 +54,86 @@
 
 		<p>Nunc ipsum arcu, mollis sit amet nulla in, interdum fringilla lectus. Curabitur laoreet, dui sed dapibus egestas, mauris urna suscipit sem, quis ullamcorper nisi lorem in nisi. Mauris vel lorem luctus, tempus massa eu, vestibulum ipsum. Sed auctor tincidunt libero a consequat. Quisque elementum nec nunc vel malesuada. Nullam tellus lacus, tincidunt sed pulvinar at, pellentesque et sapien. Duis quis urna eget sem congue sodales. Ut suscipit ante vel nulla efficitur, ut viverra erat aliquam. Sed sagittis ultricies dictum. Aliquam nec pretium est, vel interdum libero. Mauris luctus enim et condimentum scelerisque. Nulla fringilla arcu sit amet sem vulputate aliquam.</p>
 	
+
+	<!--
+		=======================================================================================================================
+		LESSON DATA RETRIEVAL BASED ON USER ID INPUT(BEING WORKED ON SLOWLY)
+		WILL HAVE MOST OF THE BIG DATABASE PULLING HERE(WAHEYYY)
+		THERE SHOULD BE AN EQUIVALENT FORM TO LET YOU PUT IN THE FOLLOWING DETAILS AS A USER:
+		languageID for now (will become language from drop down[recycle from programmer sign up])
+		a programmer ID (will find way of linking to the language ID through skills table)
+		userID for now (willconvert to user email(must be registered (will do checks dw)))
+		date idk tbh,it auto fills it if nothing is put in so up to you for now
+		that's all,I'll suss out price because it can't be user specified (new table perhaps waheyy)
+		
+
+
+	-->
+	<?php
+	if( isset( $_POST['search'] ) )
+	{
+	$emailEntry = $_POST['userID'];
+	// Step 1: Connect to DBMS
+	$conn = mysqli_connect("localhost", "root", "","ProgrammingPal") or die("Unable to connect to DBMS.");
+	// Step 2: Write and run SQL command
+	//$sql = "SELECT userID FROM users WHERE email = '$emailEntry' LIMIT 1";
+	//	$runUserID = $conn->query($sql);
+	$sql = "SELECT * FROM lessons WHERE userID = '$emailEntry'";
+			$result = $conn->query($sql);
+	
+	// Step 3: Process resulting data
+		if($result->num_rows > 0) 
+		{
+			echo "<table>";
+			echo "Lesson Bookings:";
+			echo "<tr><th>LessonID</th><th>ProgrammerID</th><th>LanguageID</th><th>UserID</th><th>Date</th><th>Price</th></tr>";
+			while($row = $result->fetch_assoc())
+			{
+				$lessonID = $row["lessonID"];
+				$programmerID = $row["programmerID"];
+				$languageID = $row["languageID"];
+				$userID = $row["userID"];
+				$date = $row["date"];
+				$price = $row["price"];
+				
+				echo "<tr><td>$lessonID</td><td>$programmerID</td><td>$languageID</td><td>$userID</td><td>$date</td><td>€$price</td></tr>";
+			}
+			echo "</table>";
+		} 
+		else 
+		{
+			echo "<h4>No results returned.</h4>";
+		}
+		$conn->close();
+	}else{
+	?>
+	<form action ="lessons.php" method="POST" id="userLessonForm">
+			<table id ='formUserID'>
+				<tr>
+					<td>	
+						<b><label for="userID">Enter UserID:</label></b> <!-- USE 3 TO TEST -->
+					</td>
+					<td>
+						<input type="text" name="userID" id="userID" tabindex="1" />
+					</td>	
+				</tr>
+				<tr>
+				<td>
+				<br/>
+					<input type ="reset" id ="reset" tabindex ="2" />
+					<input type ="submit" name="search" colspan ="2" id ="search" tabindex ="3"/>
+				</td>
+				</tr>
+			</table>
+			</form>
+	<?php
+	}
+	?>
+	<!-- 
+		===========================================================================================================================
+		-->
+
+
 		
 		</div>
 		
