085b7876f89a6d65d3087bbe73410ecc0fd2b53d
LukeOSG <lukegriffin07@gmail.com>
2018-11-20 17:42:54 +0000

    Updated forms with required tag
    -Added required tag

--- studentForm.php
+++ studentForm.php
@@ -33,18 +33,24 @@
 			
 
 		<?php
-			if( isset( $_POST['submit'] )){
-				$name = $_POST['name'];
-				$email = $_POST['email'];
-				$conn = mysqli_connect("localhost", "root", "", "ProgrammingPal") or die("Unable to connect to DBMS.");
-				$sql = "INSERT INTO users(username, email) VALUES('$name', '$email')";
-				if ($conn->query($sql) === TRUE) {
-					echo "User registered!";
-				} else {
-					echo "Error: " . $sql . "<br>" . $conn->error;
-				}
-				$conn->close();
-			}else{
+		if( isset( $_POST['submit'] ) )
+		{
+			$name = $_POST['name'];
+            $email = $_POST['email'];
+			// Step 1: Connect to DBMS
+			// mysql -u root -p
+			$conn = mysqli_connect("localhost", "root", "", "ProgrammingPal") or die("Unable to connect to DBMS.");
+			// Step 2: Write and run SQL command
+			//$sql = "INSERT INTO users(username, email) VALUES('".$_POST["name"]."', '".$_POST["email"]."')";
+			$sql = "INSERT INTO users(username, email) VALUES('$name', '$email')";
+			//mysqli_query($con,"INSERT INTO users(username, email) VALUES('$name', '$email')");
+			if ($conn->query($sql) === TRUE) {
+				echo "New record created successfully";
+			} else {
+				echo "Error: " . $sql . "<br>" . $conn->error;
+			}
+			// Step 3: Process resulting data
+		}else{
 		?>
 		<form action ="studentForm.php" method="POST" id="mainForm">
 			<table id ='formTable'>
@@ -54,7 +60,7 @@
 						<b><label for="name">Name:</label></b>
 					</td>
 					<td>
-						<input type="text" name="name" id="name" tabindex="1" />
+						<input type="text" name="name" id="name" tabindex="1" required />
 					</td>	
 				</tr>
 
@@ -63,7 +69,7 @@
 					<b><label for="email">Email:</label></b>
 					</td>
 					<td>
-						<input type="text" name="email" id="email" tabindex="2" />
+						<input type="text" name="email" id="email" tabindex="2" required/>
 					</td>
 				</tr>
 
@@ -83,13 +89,11 @@
 	
 	<div id = "content2">
 		
-		<h2></h2>
+		<h2>Testimonial</h2>
 	
-		
-		<p>Sed eget aliquet eros. Morbi condimentum scelerisque pharetra. Mauris id lectus a odio efficitur malesuada. Integer est ex, ultricies sit amet suscipit eget, pulvinar ut leo. Aliquam erat volutpat. Nullam pellentesque fringilla pellentesque. Etiam iaculis felis quis efficitur ultricies. Nunc ac tellus tellus. Cras ultricies nibh elit, convallis euismod turpis pellentesque ac. Donec non nunc dui. Aliquam erat volutpat. Morbi luctus tristique magna, quis bibendum eros ornare sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In id rutrum tortor, vestibulum placerat lectus.</p>
+		<img src="images/james.jpg" title="James"  alt="James">
 
-		<p>Nunc ipsum arcu, mollis sit amet nulla in, interdum fringilla lectus. Curabitur laoreet, dui sed dapibus egestas, mauris urna suscipit sem, quis ullamcorper nisi lorem in nisi. Mauris vel lorem luctus, tempus massa eu, vestibulum ipsum. Sed auctor tincidunt libero a consequat. Quisque elementum nec nunc vel malesuada. Nullam tellus lacus, tincidunt sed pulvinar at, pellentesque et sapien. Duis quis urna eget sem congue sodales. Ut suscipit ante vel nulla efficitur, ut viverra erat aliquam. Sed sagittis ultricies dictum. Aliquam nec pretium est, vel interdum libero. Mauris luctus enim et condimentum scelerisque. Nulla fringilla arcu sit amet sem vulputate aliquam.</p>
-	
+		<p>Sed eget aliquet eros. Morbi condimentum scelerisque pharetra. Mauris id lectus a odio efficitur malesuada. Integer est ex, ultricies sit amet suscipit eget, pulvinar ut leo. Aliquam erat volutpat. Nullam pellentesque fringilla pellentesque. Etiam iaculis felis quis efficitur ultricies. Nunc ac tellus tellus. Cras ultricies nibh elit, convallis euismod turpis pellentesque ac. Donec non nunc dui. Aliquam erat volutpat. Morbi luctus tristique magna, quis bibendum eros ornare sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In id rutrum tortor, vestibulum placerat lectus.</p>
 		
 	</div>
 	
