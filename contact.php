b70e0531a20802a3bf1c2346cabb7b286ff9dc80
NameJeff <37027239+CommanderDolphin@users.noreply.github.com>
2018-11-20 15:50:23 +0000

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Programming Pal</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="indexStyle.css">
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+		<link rel="icon" href="favicon.ico">
+		
+	</head>
+
+	<div id ="container">
+		<body>
+			
+		<div id = "header">
+			<h1>Programming Pal 
+			<sub>(Destroying our souls so you don't have to)</sub></h1>	
+	
+		</div>
+		
+		<nav>
+			<a href = "index.php">Home</a>
+			<a href = "form.php">Programmers</a>
+			<a href = "lessons.php">Lessons</a>
+			<a href = "studentForm.php">Students</a>
+			<a href = "contact.php">Contact Us</a>
+		</nav>	
+	
+        <div id = "content">
+            <h2><b>Luke O'Sullivan Griffin: <sub>(HTML/CSS)</sub> </b></h2>
+            <p>"If google chrome caches one more time"</p>
+       
+
+        </div>
+
+		<div id = "content2">
+			<h2><b>Mike Vriesema: <sub>(SQL/PHP)</sub> </b></h2>
+
+			<p>"I've really learned to cope with my constant PHP induced headache"</p>
+        </div>
+			
+		
+		
+			<div class = "clear"></div>
+			<div id = "footer">
+				<p>Programming Pal,</p>
+				<p>123 UL Street,</p>
+				<p>Co. Limerick</p>
+				<a href="mailto:programming4life@programmingpal.com" target="_top">Got a question? Send us an email!</a>
+				
+				<br/>
+				<br/>
+				<a href = "index.php">| Home |</a>
+				<a href = "form.php">| Programmers |</a>
+				<a href = "lessons.php">| Lessons |</a>
+				<a href = "studentForm.php">| Students |</a>
+				<a href = "#">| Contact Us |</a>
+			</div>
+
+		</body>	
+	</div>
+</html>
+
