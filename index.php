53da5950a8d6a9256130937f69f0b0c06620772b
LukeOSG <lukegriffin07@gmail.com>
2018-11-26 16:15:15 +0000

    Correct index
    Index

--- index.php
+++ index.php
@@ -12,85 +12,80 @@
 
 	<div id ="container">
 		<body>
-			
-		<div id = "header">
-			<h1>Programming Pal 
-			<sub>(Destroying our souls so you don't have to)</sub></h1>	
-	
-		</div>
-		
-		<nav>
-			<a href = "index.php">Home</a>
-			<a href = "form.php">Programmers</a>
-			<a href = "lessons.php">Lessons</a>
-			<a href = "studentForm.php">Students</a>
-			<a href = "contact.php">Contact Us</a>
-		</nav>	
-	
-		<div id = "content">
-		
-			<h2>Why Choose Us?</h2>
-			<p><b>Fast and efficient:</b>
-			<br/>
-			<br/>
-			We ensure all programmers respond within <b>72 hours</b> to help you with all your programming needs!</p>
-
-			<p><b>Quality Code:</b>
-			<br/>
-			<br/>
-			Our code is <b>guaranteed</b> to compile...most of the time anyway.</p>
-
-
-			<p><b>Affordable:</b>
-			<br/>
-			<br/>
-			As current students we know you have more <b>important</b> things (*cough cough*) to spend money on. We take pride that our service is affordable and you can sleep easy knowing you'll survive another semester. </p>
-
-			<p><b>"Less Stress, More Success":</b>
-			<br/>
-			<br/>
-			We all know the pain of getting the dreaded "Segmentation Fault (Core Dumped)" or the classic "java.lang.NullPointerException". Don't worry we're here to help and make sure your coding experience is as smooth as possible.</p>
-
-			<p>We cover various languages such as:<br/> <b>Java, <br/>C,<br/> PHP,<br/>HTML/CSS<br/>SQL,<br/><br/>and many more...<br/></b></p>
-
-			</div>
-			
-			<div id = "content2">
-			
-				<h2>Meet The Team</h2>
-
-			<div id = "meetTheTeam">
 				
-			
-				<img src="images/gearoid.jpg" title="gags"  alt="Gearoid"></img>
-				<img src="images/ryan.jpg" title="ryano"  alt="Ryan"></img>
-				<img src="images/conall.jpg" title="LIT"  alt="Conall"></img>
+			<div id = "header">
+				<h1>Programming Pal 
+				<sub>(Destroying our souls so you don't have to)</sub></h1>	
+		
 			</div>
-
-
-			<div id = "meetTheTeamText">
-				<h3><b>Gearoid Kirwan: (Chief Executive Officer) </b></h3>
-
-				<p>"What started as a side project has grown into something I couldn't have imagined. From living in Adare to the high rise of San Francisco I am humbled to be working with some of the best developers in the world."</p>
-
+			
+			<nav>
+				<a href = "index.php">Home</a>
+				<a href = "form.php">Programmers</a>
+				<a href = "lessons.php">Lessons</a>
+				<a href = "studentForm.php">Students</a>
+				<a href = "contact.php">Contact Us</a>
+			</nav>	
+	
+			<div id = "content" class = "contentClass">
+			
+				<h2>Why Choose Us?</h2>
+				<p><b>Fast and efficient:</b>
 				<br/>
-
-				<h3><b>Ryan O'Connor: (Lead Designer) </b></h3>
-
-				<p>"The secret to creativity is knowing how to hide your sources. Shout-out to Stack Overflow."</p>
-
 				<br/>
+				We ensure all programmers respond within <b>72 hours</b> to help you with all your programming needs!</p>
 
-				<h3><b>Conall McAteer: (Back-end Developer) </b></h3>
+				<p><b>Quality Code:</b>
+				<br/>
+				<br/>
+				Our code is <b>guaranteed</b> to compile...most of the time anyway.</p>
 
-				<p>"Transferring from LIT to UL created so many opportunities which have allowed me to be where I am today. "</p>
 
-			</div>	
-		</div>
+				<p><b>Affordable:</b>
+				<br/>
+				<br/>
+				As current students we know you have more <b>important</b> things (*cough cough*) to spend money on. We take pride that our service is affordable and you can sleep easy knowing you'll survive another semester. </p>
+
+				<p><b>"Less Stress, More Success":</b>
+				<br/>
+				<br/>
+				We all know the pain of getting the dreaded "Segmentation Fault (Core Dumped)" or the classic "java.lang.NullPointerException". Don't worry we're here to help and make sure your coding experience is as smooth as possible.</p>
+
+				<p>We cover various languages such as:<br/> <b>Java, <br/>C,<br/> PHP,<br/>HTML/CSS<br/>SQL,<br/><br/>and many more...<br/></b></p>
+
+			</div>
+				
+			<div id = "content2">
+				<h2>Meet The Team</h2>
+					<div id = "meetTheTeam">
+
+							<h3><b>Gearoid Kirwan: (Chief Executive Officer) </b></h3>	
+							<div class="clearfix">
+								<img src="images/gearoid.jpg" title="gags"  alt="Gearoid">
+								<p>"What started as a side project has grown into something I couldn't have imagined. From living in Adare to the high rise of San Francisco I am humbled to be working with some of the best developers in the world."</p>
+							</div>
+							
+
+							<h3><b>Ryan O'Connor: (Lead Designer) </b></h3>
+							<div class="clearfix">
+								<img src="images/ryan.jpg" title="ryano"  alt="Ryan">
+								<p>"The secret to creativity is knowing how to hide your sources. Shout-out to Stack Overflow."</p>
+							</div>	
+						
+							
+							<h3><b>Conall McAteer: (Back-end Developer) </b></h3>
+							<div class="clearfix">
+								<img src="images/conall.jpg" title="LIT"  alt="Conall">
+								<p>"Transferring from LIT to UL created so many opportunities which have allowed me to be where I am today."</p>
+							</div>
+							
+					</div>
+			</div>
 			
 		
 		
 			<div class = "clear"></div>
+
 			<div id = "footer">
 				<p>Programming Pal,</p>
 				<p>123 UL Street,</p>
@@ -105,7 +100,6 @@
 				<a href = "studentForm.php">| Students |</a>
 				<a href = "contact.php">| Contact Us |</a>
 			</div>
-
 		</body>	
 	</div>
 </html>
